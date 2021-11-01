4bfc43b39cc8ea787f0201789479ea06cad8b389
nikhil76-coder <nikhil180031@gmail.com>
2021-11-01 23:03:37 +0530

    message

--- server.js
+++ server.js
@@ -66,11 +66,15 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api", fileRoutes.routes);
 const path = require("path");
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 // if(process.env.NODE_ENV === "production"){
 //   app.use(express.static('client/build/index.html'));
 // }
